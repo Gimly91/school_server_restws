46c4432c84250e0c5acbdc8c876c37675412a61e
Gimly91 <alex_ert15@yahoo.com>
2014-01-12 22:48:01 +0200

    Sonar Fix

--- src/school/encrypt/MD5Encrypt.java
+++ src/school/encrypt/MD5Encrypt.java
@@ -3,10 +3,15 @@ package school.encrypt;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MD5Encrypt {
 
 	public static String hashPassword(String password) {
+
+		final Logger LOGGER = Logger.getLogger(MD5Encrypt.class.getName());
+
 		String hashword = null;
 		try {
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
@@ -14,7 +19,7 @@ public class MD5Encrypt {
 			BigInteger hash = new BigInteger(1, md5.digest());
 			hashword = hash.toString(16);
 		} catch (NoSuchAlgorithmException nsae) {
-			nsae.printStackTrace();
+			LOGGER.log(Level.SEVERE, nsae.getMessage());
 		}
 		return hashword;
 	}
