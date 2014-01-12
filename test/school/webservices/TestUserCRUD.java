3b71bd884c3a878a1b8877084981a7bb8c303e5b
Gimly91 <alex_ert15@yahoo.com>
2014-01-12 15:57:23 +0200

    Fixed Tests

--- test/school/webservices/TestUserCRUD.java
+++ test/school/webservices/TestUserCRUD.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static school.webservices.TestUtils.adminUser;
+import static school.webservices.TestUtils.*;
 import static school.webservices.TestUtils.users;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -13,12 +16,16 @@ import javax.persistence.Query;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import school.model.User;
 import school.webservice.UserCRUD;
 
 public class TestUserCRUD {
 
 	@Test
 	public void read() {
+		List<User> usersT = new ArrayList<>();
+		usersT.add(adminUser);
+		usersT.add(simpleUser);
 		EntityManager em = Mockito.mock(EntityManager.class);
 		String q = "SELECT p from " + "user" + " p where p.username = :userN";
 		Query query = Mockito.mock(Query.class);
@@ -26,18 +33,19 @@ public class TestUserCRUD {
 		UserCRUD lb = new UserCRUD(em, q);
 
 		Mockito.when(em.createQuery(q)).thenReturn(query);
-		Mockito.when(query.getResultList()).thenReturn(users);
+		Mockito.when(query.getResultList()).thenReturn(usersT);
 
 		assertEquals(adminUser, lb.read(adminUser.getUsername()));
 
-		users.remove(0);
-		users.remove(0);
+		usersT.remove(0);
+		usersT.remove(0);
 
 		assertNull(lb.read(adminUser.getUsername()));
 	}
-	
+
 	@Test
 	public void check() {
+		List<User> usersT = new ArrayList<>();
 		EntityManager em = Mockito.mock(EntityManager.class);
 		String q = "SELECT p from " + "user" + " p where p.username = :userN";
 		Query query = Mockito.mock(Query.class);
@@ -45,12 +53,12 @@ public class TestUserCRUD {
 		UserCRUD lb = new UserCRUD(em, q);
 
 		Mockito.when(em.createQuery(q)).thenReturn(query);
-		Mockito.when(query.getResultList()).thenReturn(users);
+		Mockito.when(query.getResultList()).thenReturn(usersT);
 
-		users.add(adminUser);
+		usersT.add(adminUser);
 		assertTrue(lb.checkUser(adminUser.getUsername()));
 
-		users.remove(0);
+		usersT.remove(0);
 		assertFalse(lb.checkUser(adminUser.getUsername()));
 	}
 }
